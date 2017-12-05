5bdd172f5efe16bed4ae7ac1fe67604c275f07d7
yul13 <yul13@uci.edu>
2017-12-05 13:36:26 -0800

    comments

--- res.py
+++ res.py
@@ -1,18 +1,19 @@
 from core import *
 from tools import *
+import os
+CWD=os.getcwd()
 
+mm9 = GenomeUCSCRefGene(CWD+"/data/mm9.chromInfo.txt",
+        CWD+"/data/mm9.refGene.txt")
 
-mm9 = GenomeUCSCRefGene("/baldig/biotools/annotations/UCSC/mm9/chromInfo.txt",
-        "/baldig/biotools/annotations/UCSC/mm9/refGene.txt")
+mm10 = GenomeUCSCRefGene(CWD+"/data/mm10.chromInfo.txt",
+        CWD+"/data/mm10.refGene.txt")
 
-mm10 = GenomeUCSCRefGene("/baldig/biotools/annotations/UCSC/mm10/chromInfo.txt",
-        "/baldig/biotools/annotations/UCSC/mm10/refGene.txt")
+hg19 = GenomeUCSCRefGene(CWD+"/data/hg19.chromInfo.txt",
+        CWD+"/data/hg19.refGene.txt")
 
-hg19 = GenomeUCSCRefGene("/baldig/biotools/annotations/UCSC/hg19/chromInfo.txt",
-        "/baldig/biotools/annotations/UCSC/hg19/refGene.txt")
-
-hg38 = GenomeUCSCRefGene("/baldig/biotools/annotations/UCSC/hg38/chromInfo.txt",
-        "/baldig/biotools/annotations/UCSC/hg38/refGene.txt")
+hg38 = GenomeUCSCRefGene(CWD+"/data/hg38.chromInfo.txt",
+        CWD+"/data/hg38.refGene.txt")
 
 REFS = {
         "mm9":mm9,
@@ -20,9 +21,9 @@ REFS = {
         "hg19":hg19,
         "hg38":hg38,
         }
+#the sequence db interface works with mafslice
+#mm10seq= UCSCGenomeSequence(mm10, "db/mm10_config_test.kch","mm10")
 
-mm10seq= UCSCGenomeSequence(mm10, "db/mm10_config_test.kch","mm10")
+#hg38seq= UCSCGenomeSequence(hg38, "db/hg38_config.kch","hg38")
 
-hg38seq= UCSCGenomeSequence(hg38, "db/hg38_config.kch","hg38")
-
-SEQS = {"mm10":mm10seq, "hg38":hg38seq}
+#SEQS = {"mm10":mm10seq, "hg38":hg38seq}
